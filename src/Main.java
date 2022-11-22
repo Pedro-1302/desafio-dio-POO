f2cd00e03feb0177ed55dceae27b2cf61f7887ca
PedroFranco <mrcraftsoficcial@gmail.com>
2022-11-22 18:56:48 -0300

    Atualização

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -19,14 +17,45 @@ public class Main {
 
         Conteudo conteudo = new Curso();      // Polimorfismo
         Conteudo mentoria = new Mentoria();   // Polimorfismo
-
         Mentoria mentoria1 = new Mentoria();
+
         mentoria1.setTitulo("Mentoria de Java");
         mentoria1.setDescricao("Uma mentoria de Java");
         mentoria1.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria1);
+        System.out.println(mentoria1); */
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Java Developer Bootcamp");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devPedro = new Dev();
+        devPedro.setNome("Pedro");
+        devPedro.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Pedro : " + devPedro.getConteudosInscritos());
+        devPedro.progredir();
+        devPedro.progredir();
+        devPedro.progredir();
+        System.out.println("Conteudos Inscritos Pedro : " + devPedro.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos Pedro : " + devPedro.getConteudosConcluidos());
+        System.out.println("XP: " + devPedro.calcularTotalXP());
+
+        System.out.println("\n\n");
+
+        Dev devFranco = new Dev();
+        devFranco.setNome("Franco");
+        devFranco.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Franco : " + devFranco.getConteudosInscritos());
+        devFranco.progredir();
+        System.out.println("Conteudos Inscritos Franco : " + devFranco.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos Franco : " + devFranco.getConteudosConcluidos());
+        System.out.println("XP: " + devFranco.calcularTotalXP());
+
+
     }
 }
